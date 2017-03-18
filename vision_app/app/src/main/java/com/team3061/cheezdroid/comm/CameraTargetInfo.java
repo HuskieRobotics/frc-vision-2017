ae8e69a32cd6281acad0018f7a0e83e54911e679
Brandon John <wiresboy@yahoo.com>
2017-03-18 12:25:23 -0500

    Send X position over comms

--- vision_app/app/src/main/java/com/team3061/cheezdroid/comm/CameraTargetInfo.java
+++ vision_app/app/src/main/java/com/team3061/cheezdroid/comm/CameraTargetInfo.java
@@ -6,6 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CameraTargetInfo {
+    protected double m_x;
     protected double m_y;
     protected double m_z;
 
@@ -15,6 +16,7 @@ public class CameraTargetInfo {
     // +z is to the top of the image
     // We assume the x component of all targets is +1.0 (since this is homogeneous)
     public CameraTargetInfo(double y, double z) {
+        m_x = 1.0;
         m_y = y;
         m_z = z;
     }
@@ -25,6 +27,10 @@ public class CameraTargetInfo {
             value += 1e-7;
         }
         return value;
+    }
+
+    public double getX() {
+        return m_x;
     }
 
     public double getY() {
@@ -38,6 +44,7 @@ public class CameraTargetInfo {
     public JSONObject toJson() {
         JSONObject j = new JSONObject();
         try {
+            j.put("x", doubleize(getX()));
             j.put("y", doubleize(getY()));
             j.put("z", doubleize(getZ()));
         } catch (JSONException e) {
